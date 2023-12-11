d644cbd98265e838d835bd0c6fa847e20ead3c6c
Денис Титов <superzombe4321@gmail.com>
2023-12-11 17:07:37 +0200

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+
+def home(request):
+	# проверка на вход в систему
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Авторизация
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Succesful login")
+			return redirect('home')
+		else:
+			messages.success(request, "somethink wrong, check your login and password...")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You have been logout")
+	return redirect('home')
+
+def register_user(request):
+	return render(request, 'register.html', {})
